aac74cba2f64eaeda25e58df3dc38c7a5d7f55e2
Jirat Arinrith <jepezi@gmail.com>
2017-05-21 16:58:46 +0700

    Add react

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -17,7 +17,7 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              presets: ['env', 'stage-2']
+              presets: ['env', 'stage-2', 'react']
             }
           }
         ]
--- web/index.js
+++ web/index.js
@@ -1,1 +1,12 @@
-console.warn('yay')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+  return (
+    <div>Hello</div>
+  )
+}
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
