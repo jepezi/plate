f1bfb24beb0b48ca2585ddfb30205966f9e7379c
Jirat Arinrith <jepezi@gmail.com>
2017-05-22 09:47:49 +0700

    classnames

--- web/App/App.js
+++ web/App/App.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import cx from 'classnames'
 import css from './App.module.scss'
 
 class App extends React.Component {
   render() {
+    const containerStyles = cx(
+      css.container,
+      css.padding20,
+    )
+    const headingStyle = cx(
+      css.heading1,
+      'blue',
+    )
     return (
-      <div className={css.container}>
-        <h1 className={css.heading1}>Hello</h1>
+      <div className={containerStyles}>
+        <h1 className={headingStyle}>Hello</h1>
         <div className={css.pug}>
           <div className={css.caption}>Pug is the best</div>
         </div>
