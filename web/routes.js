5c04ba7fe86fb6576b36d64a8d01d59b3880e5a0
Jirat Arinrith <jepezi@gmail.com>
2017-05-28 08:51:01 +0700

    Code splitting

--- web/routes.js
+++ web/routes.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import App from './App/App'
-import Home from './Home/Home'
-import About from './About/About'
+
+function loadRoute(cb) {
+  return (module) => cb(null, module.default)
+}
+
+function errorLoadRoute(err) {
+  console.log("Chunk loading failed", err)
+}
 
 const routes = {
   path: '/',
   component: App,
-  indexRoute: { component: Home },
+  indexRoute: {
+    // component: Home
+    getComponent(nextState, cb) {
+      import('./Home/Home').then(loadRoute(cb)).catch(errorLoadRoute)
+    },
+  },
   childRoutes: [
-    { path: 'about', component: About },
+    {
+      path: 'about',
+      // component: About
+      getComponent(nextState, cb) {
+        import('./About/About').then(loadRoute(cb)).catch(errorLoadRoute)
+      },
+    },
   ]
 }
 
