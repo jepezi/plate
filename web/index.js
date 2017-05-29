cbd280626dab9e6af2cc0619ee5c05d1e612a871
Jirat Arinrith <jepezi@gmail.com>
2017-05-29 16:58:44 +0700

    Setup redux

--- web/index.js
+++ web/index.js
@@ -3,6 +3,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {match, Router, browserHistory} from 'react-router'
 import routes from './routes'
+import {createStore} from 'redux'
+
+function counter(state = 0, action) {
+  switch (action.type) {
+  case 'INCREMENT':
+    return state + 1
+  case 'DECREMENT':
+    return state - 1
+  default:
+    return state
+  }
+}
+const store = createStore(counter)
+store.subscribe(() => console.warn(store.getState()))
+console.warn(store.getState())
+store.dispatch({type: 'INCREMENT'})
 
 match(
   { history: browserHistory, routes },
