56620a3a3e8610f24bee264fb441cab37471175f
Jirat Arinrith <jepezi@gmail.com>
2017-05-29 10:05:35 +0700

    Different state tree

--- web/About/About.js
+++ web/About/About.js
@@ -1,9 +1,20 @@
 import React, {PropTypes} from 'react';
 
-const About = (props) => {
-  return (
-    <div>About</div>
-  )
+class About extends React.Component {
+  state = {count: 0}
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <div>
+          <button onClick={this._handleClick}>Click {this.state.count}</button>
+        </div>
+      </div>
+    )
+  }
+  _handleClick = e => {
+    this.setState(s => ({count: s.count + 1}))
+  }
 }
 
 export default About
--- web/Home/Home.js
+++ web/Home/Home.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import css from './Home.module.scss'
 
-const Home = (props) => {
-  return (
-    <div>
-      <div className={css.pug}>
-        <div className={css.caption}>Pug is the best</div>
+class Home extends React.Component {
+  state = {data: null}
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts?userId=1')
+      .then(r => r.json())
+      .then(r => this.setState({data: r}))
+  }
+  render() {
+    console.warn(this.state)
+    return (
+      <div>
+        <div className={css.pug}>
+          <div className={css.caption}>Pug is the best</div>
+        </div>
+        <div>
+          {this.state.data && this.state.data.map(e => {
+            return (
+              <div key={`post_${e.title}`}>{e.title}</div>
+            )
+          })}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Home
