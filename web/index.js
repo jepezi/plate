47c4070c4ea72e040d3fe79b3f543542e64e727f
Jirat Arinrith <jepezi@gmail.com>
2017-06-11 13:39:43 +0700

    Remove unused

--- web/index.js
+++ web/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {match, Router, browserHistory} from 'react-router'
 import routes from './routes'
-import { Provider } from 'react-redux'
 import configureStore from './store/configureStore'
 import { ApolloClient, ApolloProvider, createNetworkInterface } from 'react-apollo'
 
